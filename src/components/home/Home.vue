b2b176b7921aaef5a177da64780b3d47b936138f
caozecheng1996@163.com <caozecheng1996@163.com>
2020-04-20 10:40:13 +0800

    1

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,26 +1,38 @@
 <template>
-  <div class="home-container">
-    <div class="header">
-      <div class="menu">
-        <el-dropdown>
-          <el-button type="primary"></el-button>
-          <el-dropdown-menu slot="dropdown">
-            <el-menu text-color="rgb(191, 203, 217)" active-text-color="rgb(71, 136, 255)" background-color="#071234" :default-active="activeIndex" @select="handleSelect" router>
-              <NavMenu :navMenus="menuData"></NavMenu>
-            </el-menu>
-          </el-dropdown-menu>
-        </el-dropdown>
+  <div class="flox">
+    <div class="container">
+      <div class="header">
+        <div class="header-img"></div>
+        <div class="menu">
+          <el-dropdown>
+            <el-button type="primary"></el-button>
+            <el-dropdown-menu slot="dropdown">
+              <el-menu text-color="rgb(191, 203, 217)" active-text-color="rgb(71, 136, 255)" background-color="#071234" :default-active="activeIndex" @select="handleSelect" router>
+                <NavMenu :navMenus="menuData"></NavMenu>
+              </el-menu>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </div>
+        <div class="breadcrumb">
+          <el-breadcrumb separator-class="el-icon-arrow-right">
+            <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+            <el-breadcrumb-item>油库统计</el-breadcrumb-item>
+          </el-breadcrumb>
+        </div>
       </div>
     </div>
-
   </div>
 </template>
 
 <script>
 // 导入属于自己的 mixins 文件
 import mix from "./home-mixin";
+import NavMenu from "./navMenu/NavMenu";
 export default {
   mixins: [mix],
+  components: {
+    NavMenu
+  }
 };
 </script>
 <style lang="less" scoped>
--- src/components/home/navMenu/NavMenu.vue
+++ src/components/home/navMenu/NavMenu.vue
@@ -2,7 +2,12 @@
     <div class="navMenu">
         <template v-for="navMenu in navMenus">
             <!-- 最后一级菜单 -->
-            <el-menu-item v-if="!navMenu.childs&&navMenu.entity" :key="navMenu.entity.id" :data="navMenu" :index="navMenu.entity.name" :route="navMenu.entity.name">
+            <el-menu-item 
+            v-if="!navMenu.childs&&navMenu.entity" 
+            :key="navMenu.entity.id" 
+            :data="navMenu" 
+            :index="navMenu.entity.name" 
+            :route="navMenu.entity.name">
                 <i :class="navMenu.entity.icon"></i>
                 <span slot="title">{{navMenu.entity.alias}}</span>
             </el-menu-item>
